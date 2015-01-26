4baa6ed38bba1f29da1a6c601042813fd9d6dc4c
vidal <vidal.fey@utu.fi>
2015-01-26 15:46:09 +0200

    changed directive for import of functions: bot AnnotationDbi and gdata are now marked as 'Imports' in the DESCRIPTION file 'read.xls' is called as 'gdata::read.xls' in read2list.R cranked up version to 0.1-9
    Signed-off-by: vidal <vidal.fey@utu.fi>

--- R/read2list.R
+++ R/read2list.R
@@ -62,7 +62,7 @@ read2list <-
 					if (verbose) cat(paste(" Reading ", length(sheets[[xl]]), " sheet(s)...\n", sep = ""))
 					dl <- lapply(sheets[[xl]], function(z) {
 								if (verbose) cat(paste("  Sheet ", z, "...", sep = ""))
-								xls <- rm.empty.cols(read.xls(x, sheet = z, skip=skip, ...))
+								xls <- rm.empty.cols(gdata::read.xls(x, sheet = z, skip=skip, ...))
 								if (verbose) cat("done\n")
 								if (is.null(xls[[1]])) warning("Error while reading", x,
 											"Returning", xls)
